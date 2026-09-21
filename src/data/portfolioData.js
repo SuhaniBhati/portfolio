c8318a508e329eb60a24cf29bae897e29c3267c9
Suhani Bhati <bhatisuhani5@gmail.com>
2026-09-21 17:43:43 +0530

    Configure portfolio for Vercel deployment

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -16,7 +16,7 @@ export const profile = {
 export const links = {
   linkedin: 'https://www.linkedin.com/in/suhani-bhati-aa528828b/',
   github: 'https://github.com/SuhaniBhati',
-  resume: '[PLACEHOLDER_RESUME_URL]',
+  resume: '[https://drive.google.com/file/d/1dsKbWoJP9wyyi6uPoibT6zyfMZco1ncW/view?usp=sharing]',
 }
 
 export const about = {
